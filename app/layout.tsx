33e3cb2f8dde985de3cf3f3840f97673139a2623
S-a-b-a-r-i-s-h <sabrusabarish@gmail.com>
2024-05-03 16:23:39 +0530

    lets check one more time

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
+import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Topbar from "@/components/shared/Topbar";
 import Bottombar from "@/components/shared/Bottombar";
